d666cdc0b101d4a420165dc3a043ce9eac0939be
dcolas98 <dcolas98@gmail.com>
2022-08-03 15:13:10 +0000

    working on styles

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<div className="bigDiv d-flex justify-content-center">
+			<div className="LDiv col-3"></div>
+			<div className="MDiv col-6"> 
+			<nav className="navbar navbar-light bg-light mb-3 d-flex justify-content-center">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<h1 className="appTitle navbar-brand mb-0 h1">App Name</h1>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
 		</nav>
+			</div>
+			<div className="RDiv col-3"></div>
+		</div>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,39 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+		<div className="bigDivOne d-flex justify-content-center">
+    <div className="LDiv col-3"></div>
+    <div className="MDiv col-6 d-flex justify-content-center">
+    <form>
+      <h1 className="d-flex justify-content-center" >New Account</h1>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Full Name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+  
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Confirm Email</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Confirm Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  </div>
+
+  <button type="submit" class="btn btn-primary mt-2 mb-3">Create New Account</button>
+</form>
+</div>
+<div className="RDiv col-3"></div>
+</div>
 );
