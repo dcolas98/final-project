f2bb3ff315c3e5ba99abe2ab32d3f2b070d22c81
dcolas98 <dcolas98@gmail.com>
2022-08-10 22:25:15 +0000

    styles on pg 1 done

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,33 +4,36 @@ import { Link } from "react-router-dom";
 
 export const Home = () => (
     <div className="text-center mt-5">
-        <h1>This the App Name</h1>
-        <p>
-            Welcome to (the App) where you can test your coding knowledge. Good Luck
-        </p>
-        <div>
+        <div className="login">
+          <div className="logo">
+            <img className="logo" src="https://image.shutterstock.com/image-vector/mobile-application-qr-code-scanning-600w-1803698605.jpg"/>
+            <div className="img1">
+            </div>
+          </div>
         <form>
-    <div className="epw">       
+    <div className="epw">
   <div class="form-group">
+    <div className="emailCircle"></div>
     <p>Returning Users:</p>
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <input type="email" class="emInput form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
-  <div class="form-group">
+  <div className="form-group mt-3">
+    <div className="pwSquare"></div>
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <input type="password" class="pwInput form-control" id="exampleInputPassword1"/>
   </div>
   < Link to="/userhome">
-<button type="submit" className="btn btn-primary mt-2">Submit</button>
+<button type="submit" className="btn btn-primary mt-4">Submit</button>
 </Link>
   </div>
 </form>
-<div classname="register">
+<div className="register mt-5">
     <h3>New User? Create an Account</h3>
 	<Link to="/createaccount">
     <button className="btn btn-success">Create an Account</button>
 	</ Link>
 </div>
-        </div>
-    </div>
+</div>
+</div>
 );
