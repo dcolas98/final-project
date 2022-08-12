4eb904fc334be0725c0aa206b5b719aa293cfc46
dcolas98 <dcolas98@gmail.com>
2022-08-12 22:11:29 +0000

    styles complete

--- src/js/views/userhome.js
+++ src/js/views/userhome.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 
 export const UserHome = () => {
     return (
-<div> 
-< Link to="/quiz">
-<button type="submit" className="btn btn-primary mt-2">Quiz 1</button>
-</Link>
-</div>
+        <><div className="bg"></div><div className="bg bg2"></div><div className="bg bg3"></div><div>
+            <Link to="/quiz">
+                <button type="submit" className="btn btn-primary mt-2">Quiz 1</button>
+            </Link>
+        </div></>
 )};
